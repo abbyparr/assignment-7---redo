50e6edfea1e631bc2cbf8b102532e837248543c7
abbyparr <123125559+abbyparr@users.noreply.github.com>
2023-04-10 21:49:19 -0400

    Add files via upload

---
+++ assignment 7 question 1 redo.c
@@ -0,0 +1,16 @@
+def probability(x,y,n,z):
+    n = input('input the dice configuration')
+    z = input('input numeric value')
+    
+  
+    for (x):
+        n[0] * n[2] = x
+        
+    for (probability):
+        y = x - z
+        probability = y / x
+        print('the probability is',probability)
+        
+    if (n != [3]):
+        print('the input is not a dice configuration. input a dice configuartion')
+        
